72c236dd39b9e92aa671414a02e6d0a774feab51
Vidhi Sahay <vidhisahay25@gmail.com>
2026-04-04 15:12:33 +0530

    fix: add pypdf to requirements, pre-download sentence-transformers model at build

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+pip install -r requirements.txt
+python -c "from sentence_transformers import SentenceTransformer; SentenceTransformer('all-MiniLM-L6-v2'); print('Model downloaded')"
